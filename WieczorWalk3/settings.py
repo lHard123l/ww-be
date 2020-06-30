722fc693c0fdde083325c9d048497f3ae891dc81
lHard123l <66806244+lHard123l@users.noreply.github.com>
2020-07-01 01:31:26 +0200

    Update settings.py

--- WieczorWalk3/settings.py
+++ WieczorWalk3/settings.py
@@ -98,15 +98,10 @@ WSGI_APPLICATION = 'WieczorWalk3.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'wieczorwalkdb',
-        'USER':'wieczorwalk1',
-        'PASSWORD':'Admin123',
-        'HOST':'localhost',
-        'PORT':1433
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
